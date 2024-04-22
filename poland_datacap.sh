c843053da4d5103807d5bf5f20ca77293497e61b
MarvinHongX <marvinhongx@gmail.com>
2024-04-22 16:08:27 +0900

    bashrc

--- poland_datacap.sh
+++ poland_datacap.sh
@@ -1,3 +1,11 @@
+#!/bin/bash
+
+cat $HOME/.bashrc | grep export > ./bash.sh
+cat $HOME/.bashrc | grep cargo >> ./bash.sh
+cat ./bash.sh
+. ./bash.sh
+
+
 log_file="/home/fildata/log/poland_datacap.log"
 
 current_date=$(date "+%Y-%m-%dT%H:%M:%S.%N")
