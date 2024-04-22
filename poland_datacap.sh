aadc02c11feed7342d69986897db3c2edcfedbe0
MarvinHongX <marvinhongx@gmail.com>
2024-04-22 14:35:19 +0900

    Initial commit

---
+++ poland_datacap.sh
@@ -0,0 +1,6 @@
+log_file="/home/fildata/log/poland_datacap.log"
+
+current_date=$(date "+%Y-%m-%dT%H:%M:%S.%N")
+output_tb=$(lotus filplus check-client-datacap f15nikeuukgjpk3rf3ndmxljgroagj52vdgznkcwy | awk '{ printf "%.2f TiB\n", $1 / (1024^4) }')
+echo -e "$current_date\tINFO\t$output_tb"
+echo -e "$current_date\tINFO\t$output_tb" >> "$log_file"
